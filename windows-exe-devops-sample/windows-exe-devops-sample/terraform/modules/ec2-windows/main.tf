9a79b4a86d779517d7e48928025dd145f222860c
ShriramPrakash <70899686+ShriramPrakash@users.noreply.github.com>
2026-03-20 22:46:00 +0530

    Update main.tf

--- windows-exe-devops-sample/windows-exe-devops-sample/terraform/modules/ec2-windows/main.tf
+++ windows-exe-devops-sample/windows-exe-devops-sample/terraform/modules/ec2-windows/main.tf
@@ -4,7 +4,18 @@ resource "aws_instance" "this" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   iam_instance_profile   = var.instance_profile_name
+
   associate_public_ip_address = true
+
+  user_data = <<-EOF
+              <powershell>
+              New-Item -ItemType Directory -Path "C:\\Temp" -Force | Out-Null
+              $msiPath = "C:\\Temp\\AWSCLIV2.msi"
+              Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.msi" -OutFile $msiPath
+              Start-Process msiexec.exe -ArgumentList "/i $msiPath /qn" -Wait
+              aws --version | Out-File -FilePath "C:\\Temp\\aws-cli-version.txt"
+              </powershell>
+              EOF
 
   root_block_device {
     volume_size = 50
